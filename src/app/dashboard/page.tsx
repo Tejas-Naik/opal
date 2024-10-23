239ada851d2f326c6e6643562d6b250d28afdaf8
Tejas-Naik <rntejas2005@gmail.com>
2024-10-23 13:53:24 +0530

    Created Prisma Schemas for our data

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const DashboardPage = (props: Props) => {
+    // Authentication
+
+    // if account doesnt exist
+  return (
+    <div>DashboardPage</div>
+  )
+}
+export default DashboardPage;
